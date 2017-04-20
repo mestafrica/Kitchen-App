50bfec544b3721a7dd27000e290931abf44e938a
Jerry Y <jerry@local>
2017-04-20 13:41:10 +0000

    add router for admin view

---
+++ src/components/AdminHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AdminHome extends React.Component {
+    render() {
+        return (
+            <h2>Admin Home</h2>
+        );
+    }
+}
+
+export default AdminHome;
---
+++ src/components/CreateMeal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CreateMeal extends React.Component {
+    render() {
+        return (
+            <h2>Create Meal</h2>
+        );
+    }
+}
+
+export default CreateMeal;
