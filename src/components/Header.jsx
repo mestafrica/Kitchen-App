546e53f239862e38eac6d1fa214865e6b9fd2786
Oluwole Oyekanmi <wolexzie@gmail.com>
2017-04-24 20:40:44 +0100

    Add order summary for eits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,16 @@ import {Link, NavLink} from 'react-router-dom';
 
 class Header extends React.Component {
     render() {
+
+      let headerStyle = {
+        containerStyle: {
+          width: '50%',
+          margin: '0 auto'
+        }
+      }
         return (
 
-            <div>
+            <div style={headerStyle.containerStyle}>
                 <h3>MEST Kitchen App</h3>
             </div>
         );
